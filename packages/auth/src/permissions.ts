c7401682516b6cd9967c46a5cce0d3ec9dcdd7dd
José Victor <josevictornascimento2016@gmail.com>
2024-08-11 13:16:46 -0300

    fix: optimize permissions typing

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,8 +2,7 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './subjects/roles'
 
 type PermissionsByRole = (user: User, build: AbilityBuilder<AppAbility>) => void
 
@@ -12,7 +11,8 @@ export const permissions: Record<Role, PermissionsByRole> = {
     can('manage', 'all')
   },
   MEMBER(_, { can }) {
-    can('invite', 'User')
+    // can('invite', 'User')
     can('manage', 'Project')
   },
+  BILLING() {},
 }
--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,1 +1,14 @@
-export type ProjectSubject = ['create' | 'delete' | 'manage', 'Project']
+import { z } from 'zod'
+
+export const projectSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Project'),
+])
+
+export type projectSubject = z.infer<typeof projectSubject>
